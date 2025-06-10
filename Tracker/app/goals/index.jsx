f21c62f6b3d9d6751f3b5600c4cc57bcb598b4eb
Mike Zahalan <mzahalan@amazon.com>
2025-06-10 14:24:33 -0700

    Initialized the goals array with some dummy data. Rendered a flatlist of goals on the goals page (goals/index.jsx)

--- Tracker/app/goals/index.jsx
+++ Tracker/app/goals/index.jsx
@@ -1,6 +1,7 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Pressable } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useGoals } from '../../hooks/useGoals'
+
 
 const Goals = () => {
 
@@ -9,7 +10,15 @@ const Goals = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Your Goals</Text>
-
+      <FlatList
+        data={goals}
+        keyExtractor={(item) => item.$id}
+        contentContainerStyle={styles.list}
+        renderItem={({ item }) => (
+          <Pressable>
+            <Text>{item.desc}</Text>
+          </Pressable>
+        )} />
     </SafeAreaView>
   )
 }
--- Tracker/contexts/GoalsContext.jsx
+++ Tracker/contexts/GoalsContext.jsx
@@ -3,7 +3,11 @@ import { createContext, useState } from 'react';
 export const GoalsContext = createContext()
 
 export function GoalsProvider({children}) {
-    const [goals] = useState([])
+    const [goals] = useState([
+        {$id: "1", desc: "Goal 1"},
+        {$id: "2", desc: "Goal 2"},
+        {$id: "3", desc: "Goal 3"}
+    ])
 
     async function addGoal(goal) {
         
