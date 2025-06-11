9281efe39c166895d12ca0d58a6ecab68e5a3107
Mike Zahalan <mzahalan@amazon.com>
2025-06-11 14:59:07 -0700

    Lesson 8 - Showing a Modal
    Created a Modal detail page for the goal. Shows the goal text, and gives a close button.
    
    Next step is to have an adjustable slider.

--- Tracker/app/goals/index.jsx
+++ Tracker/app/goals/index.jsx
@@ -1,10 +1,11 @@
-import { Text, StyleSheet, FlatList, Pressable, View } from 'react-native'
+import { Text, StyleSheet, FlatList, Pressable, View, Modal } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useGoals } from '../../hooks/useGoals'
+import { useState } from 'react'
 
 
 const Goals = () => {
-
+  const [selectedGoal, setSelectedGoal] = useState(null)
   const { goals } = useGoals()
 
   return (
@@ -15,13 +16,31 @@ const Goals = () => {
         keyExtractor={(item) => item.id}
         contentContainerStyle={styles.list}
         renderItem={({ item }) => (
-          <Pressable>
+          <Pressable onPress={()=>setSelectedGoal(item)}>
             <View style={styles.goal}>
               <Text style={{margin:16}}>{item.goal}</Text>
               <View style={[styles.progress, {width: `${item.progress}%`}]}/>
             </View>
           </Pressable>
         )}/>
+
+        {selectedGoal && (
+          <Modal 
+            visible={selectedGoal !== null}
+            animationType="slide"
+            onRequestClose={()=>setSelectedGoal(null)}>
+            <View style={styles.modal}>
+              <Text style={styles.title}>{selectedGoal.goal}</Text>
+              <Text>Adjust the progress of this goal:</Text>
+              <View style={styles.buttonsWrapper}>
+                <Pressable style={styles.btn} onPress={()=>setSelectedGoal(null)}>
+                  <Text style={{color: 'white'}}>Close</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        )}
+
     </SafeAreaView>
   )
 }
@@ -53,5 +72,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#21cc8d',
     minWidth: 10,
     borderRadius: 2,
+  },
+  modal: {
+    margin: 20,
+    marginTop: 100,
+    alignItems: 'center'
+  },
+  buttonsWrapper: {
+    width: '80%',
+    marginTop: 20,
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+    paddingTop: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    gap: 20
+  },
+  btn: {
+    backgroundColor: '#21cc8d',
+    padding: 16,
+    borderRadius: 8,
   }
 })
